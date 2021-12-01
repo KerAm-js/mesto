9e883d38bd88b7bef9bb1146d9b5c71fcd2332b6
Amir Kerimov <95amirk@gmail.com>
2021-12-01 09:53:12 +0300

    fix: fixed footer__copyright, header, modal, profilee , index.httml and script.js

--- script/script.js
+++ script/script.js
@@ -1,37 +1,29 @@
-let body = document.querySelector('.root');
-let modalBackdrop = document.querySelector('.modal__backdrop');
 let editBtn = document.querySelector('.profile__edit-button');
 let closeModalBtn = document.querySelector('.modal__close-button');
 let form = document.querySelector('.form');
 let modal = document.querySelector('.modal');
 let username = document.querySelector('.profile__username');
 let userAbout = document.querySelector('.profile__about');
-let usernameInput = document.querySelector('.form__input_value_username');
-let userAboutInput = document.querySelector('.form__input_value_about');
-
-usernameInput.value = username.textContent.trim();
-userAboutInput.value = userAbout.textContent.trim();
+let usernameInput = form.querySelector('.form__input_value_username');
+let userAboutInput = form.querySelector('.form__input_value_about');
 
 function showModal() {
-  modal.style.display = 'flex';
+  usernameInput.value = username.textContent.trim();
+  userAboutInput.value = userAbout.textContent.trim();
+  modal.classList.toggle('modal_opened');
 }
 
 function hideModal() {
-  modal.style.display = 'none';
+  modal.classList.toggle('modal_opened');
 }
 
 function saveUserData(event) {
   event.preventDefault()
-  let usernameInput = document.querySelector('.form__input_value_username');
-  let userAboutInput = document.querySelector('.form__input_value_about');
-
   username.textContent = usernameInput.value;
   userAbout.textContent = userAboutInput.value;
-
   hideModal();
 }
 
 editBtn.addEventListener('click', showModal);
-modalBackdrop.addEventListener('click', hideModal);
 closeModalBtn.addEventListener('click', hideModal);
 form.addEventListener('submit', saveUserData);
