318268330a64e0805870a6c841c750de7e8012a5
Amir Kerimov <95amirk@gmail.com>
2022-01-21 00:37:41 +0300

    feat: Add class Card

---
+++ scripts/Card.js
@@ -0,0 +1,40 @@
+class Card {
+  constructor(name, link, openPopup) {
+    this._name = name;
+    this._link = link;
+    this._openPopup = openPopup;
+  }
+
+  _createNewCard() {
+    this._element = document.querySelector('#element').content.cloneNode(true);
+    this._element.querySelector('.element__image').style.backgroundImage = `url(${this._link})`;
+    this._element.querySelector('.element__image').addEventListener('click', () => this._showImageModal(this._link, this._name));
+    this._element.querySelector('.element__title').textContent = this._name;
+    this._element.querySelector('.element__like-button').addEventListener('click', this._setLike);
+    this._element.querySelector('.element__delete-button').addEventListener('click', this._deleteCard);
+    return this._element;
+  }
+
+  _deleteCard(event) {
+    event.target.closest('.element').remove();
+  }
+
+  _setLike(event) {
+    event.target.classList.toggle('element__like-button_active')
+  }
+
+  _showImageModal(imageLink, placeName) {
+    const imageModal = document.querySelector('.modal_type_image');
+    imageModal.querySelector('.modal__image').src = imageLink;
+    imageModal.querySelector('.modal__image').alt = placeName;
+    imageModal.querySelector('.modal__place-name').textContent = placeName;
+    this._openPopup(imageModal);
+  }
+
+  getCard() {
+    const newCard = this._createNewCard()
+    return newCard;
+  }
+}
+
+export default Card;
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import Card from "./Card.js";
+
 const closeModalButtons = document.querySelectorAll('.modal__close-button');
 const profileEditBtn = document.querySelector('.profile__edit-button');
 const profileModal = document.querySelector('.modal_type_profile');
@@ -12,7 +14,6 @@ const addPlaceBtn = document.querySelector('.profile__add-place-button');
 const elements = document.querySelector('.elements__list');
 const placeNameInput = newPlaceForm.querySelector('.form__input_value_place-name');
 const imageLinkInput = newPlaceForm.querySelector('.form__input_value_image-link');
-const imageModal = document.querySelector('.modal_type_image');
 const modals = document.querySelectorAll('.modal');
 
 
@@ -47,22 +48,9 @@ function clearInputs(form) {
   form.reset();
 }
 
-function createNewCard(name, link) {
-  const newElement = document.querySelector('#element').content.cloneNode(true);
-  newElement.querySelector('.element__image').style.backgroundImage = `url(${link})`;
-  newElement.querySelector('.element__image').addEventListener('click', () => showImageModal(link, name));
-  newElement.querySelector('.element__title').textContent = name;
-  newElement.querySelector('.element__like-button').addEventListener('click', setLike);
-  newElement.querySelector('.element__delete-button').addEventListener('click', deleteCard);
-  return newElement;
-}
-
-function deleteCard(event) {
-  event.target.closest('.element').remove();
-}
-
 function addCard(name, link) {
-  elements.prepend(createNewCard(name, link));
+  const card = new Card(name, link, openPopup);
+  elements.prepend(card.getCard());
 }
 
 function onCreateCardHandler(event) {
@@ -82,21 +70,10 @@ initialCards.forEach(card => {
   addCard(card.name, card.link);
 })
 
-function setLike(event) {
-  event.target.classList.toggle('element__like-button_active')
-}
-
 function showProfileModal() {
   usernameInput.value = username.textContent.trim();
   userAboutInput.value = userAbout.textContent.trim();
   openPopup(profileModal);
-}
-
-function showImageModal(imageLink, placeName) {
-  imageModal.querySelector('.modal__image').src = imageLink;
-  imageModal.querySelector('.modal__image').alt = placeName;
-  imageModal.querySelector('.modal__place-name').textContent = placeName;
-  openPopup(imageModal);
 }
 
 function openPopup(element) {
